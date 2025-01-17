8bc60d0213731572a9d51abe719471d31e375247
SILVA <SILVA@DESKTOP-2U1E009>
2025-01-16 22:52:47 -0300

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            short currentNumber = 0;
+            List<int> validatedNumbers = new List<int>();
+            int total;
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine($"Digite 3 números para somá-los (um de cada vez). Range permitido: {short.MinValue} até {short.MaxValue}\n");
+
+            do
+            {
+                Console.Write("Digite os números a serem somados: ");
+                string input = Console.ReadLine();
+
+                if (short.TryParse(input, out currentNumber))
+                {
+                    if (currentNumber >= short.MinValue && currentNumber <= short.MaxValue)
+                    {
+                        validatedNumbers.Add(currentNumber);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"O número informado está fora do permitido que é {short.MinValue} até {short.MaxValue}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, digite apenas números.\n");
+                }
+            } while (validatedNumbers.Count < 3);
+
+            total = validatedNumbers .Sum();
+            Console.WriteLine("\n===== Resultado =====");
+            Console.WriteLine("A soma dos números digitados é: " + total);
+        }
+    }
+}
